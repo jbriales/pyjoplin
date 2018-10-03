368eb098be3e1836979ef8146dcda3aadf7e98b2
Jesus Briales <jesusbriales@gmail.com>
2018-10-03 12:36:59 +0200

    Refactor Config fields into a class (no external config file for now, TODO)

--- pyjoplin/config.py
+++ pyjoplin/config.py
@@ -5,14 +5,27 @@ Define global configuration variables
 """
 import os
 
-CONFIG_PATH = os.path.expanduser("~/.config/pyjoplin/")
-TEMP_PATH = "/tmp/pyjoplin/"
 
-# Ensure folders above exist
-for folder in [CONFIG_PATH, TEMP_PATH]:
-    if not os.path.exists(folder):
-        try:
-            os.makedirs(folder)
-        except OSError as exc:  # Guard against race condition
-            if exc.errno != errno.EEXIST:
-                raise
+class Config:
+    PATH_CONFIG = os.path.expanduser("~/.config/pyjoplin/")
+    PATH_TEMP = "/tmp/pyjoplin/"
+
+    # TODO: Use path to load/save config in file
+    # Example: https://github.com/adamchainz/lifelogger/blob/master/lifelogger/config.py
+    def __init__(self):
+        # self._path = path
+        # self._loaded = False
+        # self._data = {}
+
+        # Ensure config path folders exist
+        for folder in [self.CONFIG_PATH, self.TEMP_PATH]:
+            if not os.path.exists(folder):
+                try:
+                    os.makedirs(folder)
+                except OSError as exc:  # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+
+
+# Create global config variable
+config = Config()
--- pyjoplin/utils.py
+++ pyjoplin/utils.py
@@ -5,11 +5,11 @@ Various miscellanea utils kept here for clarity
 from datetime import datetime
 import os
 
-from pyjoplin.config import TEMP_PATH
+from pyjoplin.config import config
 
 
 def generate_tempfile_name(prefix, suffix):
     now = datetime.now().isoformat()
     # message_filename = os.path.join(MSG_PATH, 'ENTRY_MSG_'+start_str)
     # TODO: Turn this into an option instead
-    return os.path.join(TEMP_PATH, prefix + now + suffix)
+    return os.path.join(config.PATH_TEMP, prefix + now + suffix)
