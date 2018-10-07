8b2e9ffc1506bfeecf24b25b3e52cd1401d928f1
Jesus Briales <jesusbriales@gmail.com>
2018-10-07 15:07:51 +0200

    Add synonym rnd

--- pyjoplin/synonyms.py
+++ pyjoplin/synonyms.py
@@ -3,5 +3,5 @@
 # For now just a store of useful synonyms for my search
 # Check "Improve tokenizer to support synonyms with custom equivalence lists" for details on what to do with this
 synonyms = [
-    {'rand', 'random'},
+    {'rnd', 'rand', 'random'},
 ]
