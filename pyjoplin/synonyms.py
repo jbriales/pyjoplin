db0ba04db7cbff7976f66803be4be32fb644af0f
Jesus Briales <jesusbriales@gmail.com>
2018-10-07 15:07:10 +0200

    Start list of synonyms for search

---
+++ pyjoplin/synonyms.py
@@ -0,0 +1,7 @@
+"""Lists of synonyms for pyjoplin search"""
+
+# For now just a store of useful synonyms for my search
+# Check "Improve tokenizer to support synonyms with custom equivalence lists" for details on what to do with this
+synonyms = [
+    {'rand', 'random'},
+]
