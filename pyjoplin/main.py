3262b92670cd85a6f4be720be766187b68ce66ce
Jesus Briales <jesusbriales@gmail.com>
2019-01-18 12:08:00 +0000

    Add autocompletion for pyjoplin

--- pyjoplin/main.py
+++ pyjoplin/main.py
@@ -4,7 +4,7 @@
 Interface Joplin via python
 """
 
-import argparse
+import argcomplete, argparse
 import six
 import sys
 from termcolor import colored
@@ -116,104 +116,109 @@ def test():
 
     print("Complete test succeeded!")
 
-    
-def main():
-    parser = argparse.ArgumentParser(
+
+
+parser = argparse.ArgumentParser(
         description=__doc__,
         epilog="By Jesus Briales"
     )
-    subparsers = parser.add_subparsers()
+subparsers = parser.add_subparsers()
 
-    setup.parser = subparsers.add_parser(
-        'setup', description=setup.__doc__)
-    setup.parser.set_defaults(func=setup)
+setup.parser = subparsers.add_parser(
+    'setup', description=setup.__doc__)
+setup.parser.set_defaults(func=setup)
 
-    rebuild_fts_index.parser = subparsers.add_parser(
-        'rebuild_fts_index', description=rebuild_fts_index.__doc__)
-    rebuild_fts_index.parser.set_defaults(func=rebuild_fts_index)
+rebuild_fts_index.parser = subparsers.add_parser(
+    'rebuild_fts_index', description=rebuild_fts_index.__doc__)
+rebuild_fts_index.parser.set_defaults(func=rebuild_fts_index)
 
-    new.parser = subparsers.add_parser(
-        'new', description=new.__doc__)
-    new.parser.set_defaults(func=new)
-    new.parser.add_argument(
-        'title',
-        help="Title string"
-    )
-    new.parser.add_argument(
-        '--notebook',
-        default='search',
-        help="Destination notebook"
-    )
+new.parser = subparsers.add_parser(
+    'new', description=new.__doc__)
+new.parser.set_defaults(func=new)
+new.parser.add_argument(
+    'title',
+    help="Title string"
+)
+new.parser.add_argument(
+    '--notebook',
+    default='search',
+    help="Destination notebook"
+)
 
-    edit.parser = subparsers.add_parser(
-        'edit', description=edit.__doc__)
-    edit.parser.set_defaults(func=edit)
-    edit.parser.add_argument(
-        'uid',
-        help="Note uid (docid)"
-    )
+edit.parser = subparsers.add_parser(
+    'edit', description=edit.__doc__)
+edit.parser.set_defaults(func=edit)
+edit.parser.add_argument(
+    'uid',
+    help="Note uid (docid)"
+)
 
-    find_title.parser = subparsers.add_parser(
-        'find-title', description=find_title.__doc__)
-    find_title.parser.set_defaults(func=find_title)
-    find_title.parser.add_argument(
-        'title',
-        help="Note title to search"
-    )
+find_title.parser = subparsers.add_parser(
+    'find-title', description=find_title.__doc__)
+find_title.parser.set_defaults(func=find_title)
+find_title.parser.add_argument(
+    'title',
+    help="Note title to search"
+)
 
-    imfeelinglucky.parser = subparsers.add_parser(
-        'imfeelinglucky', description=imfeelinglucky.__doc__)
-    imfeelinglucky.parser.set_defaults(func=imfeelinglucky)
-    imfeelinglucky.parser.add_argument(
-        'uid',
-        help="Note uid (docid)"
-    )
+imfeelinglucky.parser = subparsers.add_parser(
+    'imfeelinglucky', description=imfeelinglucky.__doc__)
+imfeelinglucky.parser.set_defaults(func=imfeelinglucky)
+imfeelinglucky.parser.add_argument(
+    'uid',
+    help="Note uid (docid)"
+)
 
-    new_and_edit.parser = subparsers.add_parser(
-        'new_and_edit', description=new_and_edit.__doc__)
-    new_and_edit.parser.set_defaults(func=new_and_edit)
-    new_and_edit.parser.add_argument(
-        'title',
-        help="Title string"
-    )
-    new_and_edit.parser.add_argument(
-        '--notebook',
-        default='search',
-        help="Destination notebook"
-    )
+new_and_edit.parser = subparsers.add_parser(
+    'new_and_edit', description=new_and_edit.__doc__)
+new_and_edit.parser.set_defaults(func=new_and_edit)
+new_and_edit.parser.add_argument(
+    'title',
+    help="Title string"
+)
+new_and_edit.parser.add_argument(
+    '--notebook',
+    default='search',
+    help="Destination notebook"
+)
 
-    delete.parser = subparsers.add_parser(
-        'delete', description=delete.__doc__)
-    delete.parser.set_defaults(func=delete)
-    delete.parser.add_argument(
-        'uid',
-        help="Note uid (docid)"
-    )
+delete.parser = subparsers.add_parser(
+    'delete', description=delete.__doc__)
+delete.parser.set_defaults(func=delete)
+delete.parser.add_argument(
+    'uid',
+    help="Note uid (docid)"
+)
 
-    test.parser = subparsers.add_parser(
-        'test', description=test.__doc__)
-    test.parser.set_defaults(func=test)
+test.parser = subparsers.add_parser(
+    'test', description=test.__doc__)
+test.parser.set_defaults(func=test)
 
-    toy.parser = subparsers.add_parser(
-        'toy', description=toy.__doc__)
-    toy.parser.add_argument(
-        'search_terms',
-        type=six.text_type,
-        nargs='*',
-        help="The query for the search."
-    )
-    toy.parser.set_defaults(func=toy)
+toy.parser = subparsers.add_parser(
+    'toy', description=toy.__doc__)
+toy.parser.add_argument(
+    'search_terms',
+    type=six.text_type,
+    nargs='*',
+    help="The query for the search."
+)
+toy.parser.set_defaults(func=toy)
 
-    find_empty_notes.parser = subparsers.add_parser(
-        'find_empty', description=find_empty_notes.__doc__
-    )
-    find_empty_notes.parser.set_defaults(func=find_empty_notes)
-    find_empty_notes.parser.add_argument(
-        "--delete",
-        help="Delete found empty notes",
-        action='store_true'
-    )
+find_empty_notes.parser = subparsers.add_parser(
+    'find_empty', description=find_empty_notes.__doc__
+)
+find_empty_notes.parser.set_defaults(func=find_empty_notes)
+find_empty_notes.parser.add_argument(
+    "--delete",
+    help="Delete found empty notes",
+    action='store_true'
+)
 
+
+argcomplete.autocomplete(parser)
+
+    
+def main():
     if len(sys.argv) <= 1:
         parser.print_help()
         return True
@@ -224,7 +229,7 @@ def main():
     func = kwargs.pop('func')
 
     successful = func(**kwargs)
-    # sys.exit(0 if successful else 1)
+    sys.exit(0 if successful else 1)
 
 
 if __name__ == '__main__':
