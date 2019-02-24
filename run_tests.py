b300c7526b37a5e4bfb31ba734077ec34cabff9a
Jesus Briales <jesusbriales@gmail.com>
2019-02-24 17:30:01 +0000

    Remove note for legacy test

--- run_tests.py
+++ run_tests.py
@@ -10,10 +10,3 @@ if __name__ == '__main__':
     test_loader = unittest.defaultTestLoader.discover('.')
     test_runner = unittest.TextTestRunner()
     test_runner.run(test_loader)
-
-    # NOTE: Legacy, to be refactored into unit tests
-    # Just call main test from here
-    # subprocess.call("pyjoplin test", shell=True)
-
-
-
