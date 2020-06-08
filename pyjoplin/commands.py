e9c767bd7c66736b4175eb2b5e289965c30da9d3
Jesus Briales <jesusbriales@gmail.com>
2020-06-08 13:28:03 +0200

    Add synonym logic (loading from file), contaminated by format changes

--- pyjoplin/commands.py
+++ pyjoplin/commands.py
@@ -11,6 +11,10 @@ from pyjoplin.configuration import config
 from pyjoplin import notification
 from pyjoplin.utils import time_joplin
 
+from pkg_resources import resource_filename
+
+PATH_SYNONYMS = resource_filename("pyjoplin", "synonyms.txt")
+
 
 def rebuild_fts_index():
     """
@@ -31,11 +35,13 @@ def rebuild_fts_index():
     notes = Note.select()
     with db.atomic():
         for note in notes:
-            NoteIndex.insert({
-                NoteIndex.uid: note.id,
-                NoteIndex.title: note.title,
-                NoteIndex.body: note.body
-            }).execute()
+            NoteIndex.insert(
+                {
+                    NoteIndex.uid: note.id,
+                    NoteIndex.title: note.title,
+                    NoteIndex.body: note.body,
+                }
+            ).execute()
 
     if config.DO_NOTIFY:
         notification.show("Rebuilt index", message="FTS index populated from scratch")
@@ -54,18 +60,18 @@ def find_empty_notes(delete=False):
     with db.atomic():
         for note in notes:
             if not note.body:
-                print('%s %s' % (note.id, note.title))
+                print("%s %s" % (note.id, note.title))
                 if delete:
                     note.delete_instance()
-                    print('Deleted')
+                    print("Deleted")
 
 
 def rename_conflicting_notes():
     notes = Note.select().where(Note.is_conflict == 1)
     with db.atomic():
         for note in notes:
-            if not note.title.endswith('(CONFLICT)'):
-                note.title = note.title + ' (CONFLICT)'
+            if not note.title.endswith("(CONFLICT)"):
+                note.title = note.title + " (CONFLICT)"
                 num_saved_notes = note.save()
                 if num_saved_notes != 1:
                     notification.show_error("Renaming conflicting note", note.title)
@@ -82,29 +88,60 @@ def list_conflicts():
     print("List of conflicting notes:")
     with db.atomic():
         for note in notes:
-            print('-----------------------------')
-            print('%s %s' % (note.id, note.title))
+            print("-----------------------------")
+            print("%s %s" % (note.id, note.title))
 
 
 def substitute_search_column_aliases(search_str):
     import re
+
     # Substitute 't:' (after beginning of string or space) by 'title:'
-    search_str = re.sub(r'(^| )t:', r'\1title:', search_str)
+    search_str = re.sub(r"(^| )t:", r"\1title:", search_str)
     # Substitute 'b:' (after beginning of string or space) by 'body:'
-    search_str = re.sub(r'(^| )b:', r'\1body:', search_str)
+    search_str = re.sub(r"(^| )b:", r"\1body:", search_str)
+    return search_str
+
+
+def replace_synonyms(search_str):
+    # Define list of sets of synonyms
+    # These synonyms help ensuring I get all relevant results
+    # even if I may often switch between several close alternatives
+    # This avoids having to write down certain kw just for search once and again
+    with open(PATH_SYNONYMS, "r") as f:
+        sets_of_synonyms = [the_line.strip()[1:-1].split(r"' '") for the_line in f]
+    # NOTE: These are not sets but lists, but anyway keeping some name
+
+    # Preprocess into dictionary for faster query
+    dict_of_synonyms = {
+        the_word: the_set for the_set in sets_of_synonyms for the_word in the_set
+    }
+
+    pattern: str = r"(%s)" % "|".join(dict_of_synonyms.keys())
+
+    import re
+
+    # Replace any registered synonym into OR-set for SQLite query
+    # e.g. py turns into ( py OR python )
+    # return re.sub(
+    #     pattern,
+    #     lambda m: r"(%s)" % r" OR ".join(dict_of_synonyms[m.group(1)]),
+    #     search_str,
+    # )
     return search_str
 
 
 def search(search_str):
     search_str = substitute_search_column_aliases(search_str)
 
+    # Replace synonyms with all their equivalents in OR search
+    search_str = replace_synonyms(search_str)
+
     # Search query in the FTS table
     with db.atomic():
         found_index_notes = (
-            NoteIndex
-            .select(
+            NoteIndex.select(
                 NoteIndex,
-                fn.snippet(Entity(NoteIndex._meta.table_name)).alias('snippet')
+                fn.snippet(Entity(NoteIndex._meta.table_name)).alias("snippet"),
             )
             .where(NoteIndex.match(search_str))
             .order_by(NoteIndex.bm25())
@@ -122,11 +159,9 @@ def get_notes_by_id(ids, ordered=False):
 
     if ordered:
         # Order query list in same order as uids
-        dict_query = {note['id']: note for note in query}
+        dict_query = {note["id"]: note for note in query}
         # Grab note only if in the query
-        ordered_notes = [dict_query[id]
-                         for id in ids
-                         if id in dict_query]
+        ordered_notes = [dict_query[id] for id in ids if id in dict_query]
         query = ordered_notes
         # Debug: [note['id'] for note in query]
 
@@ -140,16 +175,20 @@ def setup():
     :return:
     """
     # Substitute CLI database by symbolic link to desktop database.sqlite
-    subprocess.call("rm database.sqlite", shell=True,
-                    cwd=os.path.expanduser('~/.config/joplin/'))
-    subprocess.call("ln -s ../joplin-desktop/database.sqlite", shell=True,
-                    cwd=os.path.expanduser('~/.config/joplin/'))
+    subprocess.call(
+        "rm database.sqlite", shell=True, cwd=os.path.expanduser("~/.config/joplin/")
+    )
+    subprocess.call(
+        "ln -s ../joplin-desktop/database.sqlite",
+        shell=True,
+        cwd=os.path.expanduser("~/.config/joplin/"),
+    )
 
     if config.DO_NOTIFY:
         notification.show("Setup succeeded")
 
 
-def new(title, notebook_name='search', body=''):
+def new(title, notebook_name="search", body=""):
     """
     Create new note in notebook
     :param title:
@@ -168,7 +207,8 @@ def new(title, notebook_name='search', body=''):
     # Get unique identifier following Joplin format
     # Source: https://github.com/laurent22/joplin/issues/305#issuecomment-374123414
     import uuid
-    uid = str(uuid.uuid4()).replace('-', '')
+
+    uid = str(uuid.uuid4()).replace("-", "")
     # Get current time in Joplin format (int epoch in milliseconds)
     uint_current_timestamp_msec = time_joplin()
     new_note = Note(
@@ -176,12 +216,12 @@ def new(title, notebook_name='search', body=''):
         created_time=uint_current_timestamp_msec,
         id=uid,
         parent=notebook.id,
-        source='pyjoplin',
-        source_application='pyjoplin',
+        source="pyjoplin",
+        source_application="pyjoplin",
         title=title,
         updated_time=uint_current_timestamp_msec,
         user_created_time=uint_current_timestamp_msec,
-        user_updated_time=uint_current_timestamp_msec
+        user_updated_time=uint_current_timestamp_msec,
     )
     num_saved_notes = new_note.save(force_insert=True)
     if num_saved_notes != 1:
@@ -194,8 +234,8 @@ def new(title, notebook_name='search', body=''):
 
 def edit(uid):
     subprocess.Popen(
-        'increase_hit_history_for pyjoplin %s' % inspect.currentframe().f_code.co_name,
-        shell=True
+        "increase_hit_history_for pyjoplin %s" % inspect.currentframe().f_code.co_name,
+        shell=True,
     )
 
     # Find note entry by uid
@@ -205,15 +245,17 @@ def edit(uid):
     init_body = note.body
 
     # Populate temporary file from note content
-    path_sentinel = os.path.join(config.PATH_TEMP, '%s' % uid)
+    path_sentinel = os.path.join(config.PATH_TEMP, "%s" % uid)
     if os.path.exists(path_sentinel):
         notification.show_error("Note is already under edit", note.title, note.id)
-        raise Exception('Note is already under edit')
+        raise Exception("Note is already under edit")
     else:
         # Create sentinel to block this note
-        open(path_sentinel, 'a').close()
+        open(path_sentinel, "a").close()
 
-    path_tempfile = os.path.join(config.PATH_TEMP, '%s.md' % note.title.replace('/', '_'))
+    path_tempfile = os.path.join(
+        config.PATH_TEMP, "%s.md" % note.title.replace("/", "_")
+    )
 
     note.to_file(path_tempfile)
 
@@ -232,23 +274,25 @@ def edit(uid):
         #     'bash -c "source ~/.bashrc && unset PYTHONPATH && vimx \'{path}\' || vi \'{path}\'"'.format(path=path_tempfile)
         # ],
         [
-            'urxvt256c',
-            '-title',
-            'pyjoplin - {title}'.format(title=note.title),
-            '-e',
-            'bash',
-            '-c',
+            "urxvt256c",
+            "-title",
+            "pyjoplin - {title}".format(title=note.title),
+            "-e",
+            "bash",
+            "-c",
             # NOTE: `source ~/.bashrc` is necessary to customize interactive shell for vim
             # e.g. to disable "flow control characters"
             # NOTE: `unset PYTHONPATH` seems necessary for this to work when called through ulauncher extension
-            'source ~/.bashrc && unset PYTHONPATH && vimx \'{path}\''.format(path=path_tempfile)
+            "source ~/.bashrc && unset PYTHONPATH && vimx '{path}'".format(
+                path=path_tempfile
+            )
             # NOTE: Version below works when called from terminal,
             # e.g. `pyjoplin edit 170b3c8de5034f7c8023a6a39f02219c`
             # but it immediately exits when called via ulauncher
             # 'vimx',
             # '{path}'.format(path=path_tempfile)
         ],
-        stdout=subprocess.PIPE
+        stdout=subprocess.PIPE,
     )
     # cmd_str = config.EDITOR_CALL_TEMPLATE.format(title=note.title, path=path_tempfile)
     # proc = subprocess.Popen(
@@ -264,6 +308,7 @@ def edit(uid):
 
     # Loop during edition to save incremental changes
     import time
+
     last_modification_time_sec = os.path.getmtime(path_tempfile)
     while proc.poll() is None:
         time.sleep(0.5)
@@ -273,7 +318,9 @@ def edit(uid):
             note.from_file(path_tempfile)
             num_saved_notes = note.save()
             if num_saved_notes != 1:
-                notification.show_error("Saving note changes during edition", note.title)
+                notification.show_error(
+                    "Saving note changes during edition", note.title
+                )
             # else:
             #     notification.show("Note saved", note.title)
 
@@ -281,9 +328,9 @@ def edit(uid):
     if returncode != 0:
         print("ERROR during edition")
         print("Output:")
-        output = proc.stdout.read().decode('utf-8')
+        output = proc.stdout.read().decode("utf-8")
         print(output)
-        raise Exception('ERROR during edition')
+        raise Exception("ERROR during edition")
 
     # Save edited file content to Notes table
     note.from_file(path_tempfile)
@@ -317,18 +364,20 @@ def edit(uid):
 def find_title(title):
     if isinstance(title, list):
         # For case coming from CLI
-        title = ' '.join(title)
+        title = " ".join(title)
     try:
         note = Note.get(Note.title == title)
         try:
             notebook = Folder.get(Folder.id == note.parent)
         except Folder.DoesNotExist:
-            notification.show_error("Notebook not found", message='nb id %s' % note.parent)
+            notification.show_error(
+                "Notebook not found", message="nb id %s" % note.parent
+            )
             raise Folder.DoesNotExist
 
         print(note.id)
         print(note.title)
-        print("#"+notebook.title)
+        print("#" + notebook.title)
         print(note.body)
     except Note.DoesNotExist:
         print("No exact match found for title query " + title)
@@ -337,7 +386,7 @@ def find_title(title):
 def edit_by_title(title):
     if isinstance(title, list):
         # For case coming from CLI
-        title = ' '.join(title)
+        title = " ".join(title)
     try:
         note = Note.get(Note.title == title.rstrip())
         edit(note.id)
@@ -362,8 +411,8 @@ def imfeelinglucky(uid):
     :return:
     """
     subprocess.Popen(
-        'increase_hit_history_for pyjoplin %s' % inspect.currentframe().f_code.co_name,
-        shell=True
+        "increase_hit_history_for pyjoplin %s" % inspect.currentframe().f_code.co_name,
+        shell=True,
     )
 
     # NOTE: For now only implements searching first code stub in Solution:
@@ -374,6 +423,7 @@ def imfeelinglucky(uid):
 
     # Parse first code stub (if any)
     import re
+
     stub = ""
     if not stub:
         # Typical example:
@@ -400,13 +450,13 @@ def imfeelinglucky(uid):
         edit(uid)
         return None
     # Strip newlines to avoid unexpected execution e.g. in shell
-    stub = stub.strip('\n')
+    stub = stub.strip("\n")
 
     # Copy stub into the clipboard
     # NOTE: Using xclip because pyperclip does not work
     # Create temporary file with text
-    path_tempfile = os.path.join(config.PATH_TEMP, 'stub')
-    with open(path_tempfile, 'w') as f:
+    path_tempfile = os.path.join(config.PATH_TEMP, "stub")
+    with open(path_tempfile, "w") as f:
         f.write(stub)
     cmd = "xclip -selection clipboard %s" % path_tempfile
     subprocess.call(cmd, shell=True)
@@ -423,7 +473,7 @@ def delete(uid):
         notification.show("Deleted note", note.title)
 
 
-def new_and_edit(title, notebook='search'):
+def new_and_edit(title, notebook="search"):
     # Create new entry via Joplin CLI
     new_uid = new(title, notebook)
     # Edit note
@@ -438,7 +488,7 @@ def toy(search_terms):
     # Search query in the FTS table
     # search_str = 'editor note'
     # search_str = 'sqlite python'
-    search_str = ' '.join(search_terms)
+    search_str = " ".join(search_terms)
     found_index_notes = search(search_str)
     # NOTE: This should be enough to show found entries in jlauncher
     print("FTS: %d notes found" % found_index_notes.count())
