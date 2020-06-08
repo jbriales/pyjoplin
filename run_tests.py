9b65a9ec4608f6936f6f03edc60ecd8df88e7dbb
Jesus Briales <jesusbriales@gmail.com>
2020-06-08 13:32:40 +0200

    Update run_tests to Python 3 and black-format

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # coding=utf-8
 """
 Run unittest discover to execute all tests in the package hierarchy
@@ -6,7 +6,7 @@ Run unittest discover to execute all tests in the package hierarchy
 import unittest
 import subprocess
 
-if __name__ == '__main__':
-    test_loader = unittest.defaultTestLoader.discover('.')
+if __name__ == "__main__":
+    test_loader = unittest.defaultTestLoader.discover(".")
     test_runner = unittest.TextTestRunner()
     test_runner.run(test_loader)
