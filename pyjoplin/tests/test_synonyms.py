35e9892bc054d3c4713e815aa013ad440bb5bf32
Jesus Briales <jesusbriales@gmail.com>
2020-06-08 16:44:10 +0200

    Rename tests

--- pyjoplin/tests/test_synonyms.py
+++ pyjoplin/tests/test_synonyms.py
@@ -6,13 +6,14 @@ from pyjoplin.models import Folder, Note, NoteIndex
 
 
 class TestSynonyms(unittest.TestCase):
-    def test_py(self):
+    def test_minimal_kw(self):
         the_input = "py"
         the_output = commands.replace_synonyms(the_input)
         expected_output = "(py OR python)"
         self.assertEqual(the_output, expected_output)
 
-    def test_python(self):
+    def test_kw_contained_in_str(self):
+        # py is in the set of keywords and contained as subword of python
         the_input = "python"
         the_output = commands.replace_synonyms(the_input)
         expected_output = "(py OR python)"
