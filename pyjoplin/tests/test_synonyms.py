b9dff0c0dd142a63dd87a4d7cc9dec7404ad2ee1
Jesus Briales <jesusbriales@gmail.com>
2020-06-10 12:36:35 +0200

    Add unit tests for multiple kws

--- pyjoplin/tests/test_synonyms.py
+++ pyjoplin/tests/test_synonyms.py
@@ -16,7 +16,31 @@ class TestSynonyms(unittest.TestCase):
         # py is in the set of keywords and contained as subword of python
         the_input = "python"
         the_output = commands.replace_synonyms(the_input)
-        expected_output = "(py OR python)"
+        expected_output = "(python OR python)"
+        self.assertEqual(the_output, expected_output)
+
+    def test_nonkw_kw(self):
+        the_input = "foo bc"
+        the_output = commands.replace_synonyms(the_input)
+        expected_output = "foo (BetterCalib OR bc)"
+        self.assertEqual(the_output, expected_output)
+
+    def test_two_kws(self):
+        the_input = "py bc"
+        the_output = commands.replace_synonyms(the_input)
+        expected_output = "(python OR py) (BetterCalib OR bc)"
+        self.assertEqual(the_output, expected_output)
+
+    def test_several_kws(self):
+        the_input = "py bc anal dict doc"
+        the_output = commands.replace_synonyms(the_input)
+        expected_output = (
+            "(python OR py) "
+            "(BetterCalib OR bc) "
+            "(analytics OR anal) "
+            "(dictionary OR dict) "
+            "(document OR doc)"
+        )
         self.assertEqual(the_output, expected_output)
 
 
