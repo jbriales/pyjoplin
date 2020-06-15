480212d4c9c8361924a99edf18a8b6c37384a11e
Jesus Briales <jesusbriales@gmail.com>
2020-06-15 20:25:43 +0200

    Try-except for notify2.init to circumvent notification issues

--- pyjoplin/notification.py
+++ pyjoplin/notification.py
@@ -10,35 +10,39 @@ from pyjoplin.configuration import config
 
 
 # Start notification service for pyjoplin
-if os.path.isfile(os.path.expanduser('~/var/pyjoplin/SKIP_NOTIFICATIONS')):
+if os.path.isfile(os.path.expanduser("~/var/pyjoplin/SKIP_NOTIFICATIONS")):
     # NOTE:
     # `mkdir -p ~/var/pyjoplin && touch ~/var/pyjoplin/SKIP_NOTIFICATIONS` to disable notifications
     # `rm ~/var/pyjoplin/SKIP_NOTIFICATIONS` to enable again
-    print('Skipping notify2 setup because ~/var/pyjoplin/SKIP_NOTIFICATIONS exists', file=sys.stderr)
+    print(
+        "Skipping notify2 setup because ~/var/pyjoplin/SKIP_NOTIFICATIONS exists",
+        file=sys.stderr,
+    )
 else:
-    notify2.init("pyjoplin")
+    try:
+        notify2.init("pyjoplin")
+    except:
+        print("Skipping notify2 setup because notifications service seems broken")
 
 
-def show(summary, note_title='', message=''):
+def show(summary, note_title="", message=""):
     if note_title:
         full_message = "Note: %s\n%s" % (note_title, message)
     else:
         full_message = message
     notification = notify2.Notification(
-        summary,
-        message=full_message,
-        icon=config.PATH_ICON
+        summary, message=full_message, icon=config.PATH_ICON
     )
     try:
         notification.show()
     except notify2.UninittedError:
-        print('Skipping notification because notify2.init() was not run successfully')
+        print("Skipping notification because notify2.init() was not run successfully")
 
 
-def show_error(summary, note_title='', message=''):
+def show_error(summary, note_title="", message=""):
     show("ERROR: %s" % summary, note_title, message)
 
 
-def show_error_and_raise(summary, note_title='', message=''):
+def show_error_and_raise(summary, note_title="", message=""):
     show_error(summary, note_title, message)
     raise Exception(summary)
