b5005c34531432dec319b6442f0eb6ffb9e47585
Jesus Briales <jesusbriales@gmail.com>
2021-10-18 09:53:15 +0200

    Update test

--- pyjoplin/tests/test_edit_note.py
+++ pyjoplin/tests/test_edit_note.py
@@ -5,11 +5,14 @@ from pyjoplin import commands
 
 
 class TestNewNote(unittest.TestCase):
-
     @unittest.skip("for manual debug")
     def test_edit_note_with_body(self):
-        test_body = 'foo body'
-        testnote_id = commands.new('foo test', 'test', body=test_body)
+        test_title = "foo test"
+        test_body = "foo body"
+        # HACK: Find existing to robustify against previous non-cleaned unit test
+        testnote_id = commands.find_title(test_title) or commands.new(
+            test_title, "test", body=test_body
+        )
 
         # Edit to manually debug
         commands.edit(testnote_id)
@@ -17,5 +20,5 @@ class TestNewNote(unittest.TestCase):
         commands.delete(testnote_id)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
